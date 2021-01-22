982303832d113bc844f55f78767ddc27f6f6bc8b
George Yoo <gcyoosf@gmail.com>
2021-01-22 15:15:17 -0500

    adds roles and shifts components, broken out but still static

--- p2-RESTon-angular/src/app/modules/shared-components/shared-components.module.ts
+++ p2-RESTon-angular/src/app/modules/shared-components/shared-components.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ScheduleCalanderComponent } from 'src/app/shared/schedule-calander/schedule-calander.component';
+import { RolesComponent } from 'src/app/shared/roles/roles.component';
+import { ShiftsComponent } from 'src/app/shared/shifts/shifts.component';
 
 
 
 @NgModule({
   declarations: [
     ScheduleCalanderComponent,
+    RolesComponent,
+    ShiftsComponent
   ],
   imports: [
     CommonModule,
