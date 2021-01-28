9c0c90361f915afef278e0d9f130eb45d63b6a07
will.h <64234764+chh14025@users.noreply.github.com>
2021-01-28 10:44:58 -0500

    profile functionality complete

--- p2-RESTon-angular/src/app/pages/user/user/user.component.ts
+++ p2-RESTon-angular/src/app/pages/user/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
@@ -13,8 +14,10 @@ export class UserComponent implements OnInit {
   // user: User = JSON.parse(atob(localStorage.getItem("user")));
   user: User;
   myForm: FormGroup;
+  message:boolean = false;
 
-  constructor(private authService: AuthService, private fb: FormBuilder, private userSerivce: UserService) { }
+
+  constructor(private authService: AuthService, private fb: FormBuilder, private userSerivce: UserService, private _snackBar: MatSnackBar) { }
 
 
   ngOnInit(): void {
@@ -71,6 +74,9 @@ export class UserComponent implements OnInit {
     this.userSerivce.updateUser(this.user)
     .then(user => {
       console.log(user);
+      this._snackBar.open("Profile update successful", "Dismiss", {
+        duration: 3000,
+      })
     }).catch(error => {
       console.log(error);
     });
